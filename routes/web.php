5b9318e9e6e4dc7cdac5bbe6c4ace4ab8379f5c8
christanplaza <cosmotechnologist@gmail.com>
2019-06-20 14:34:00 +0800

    [SELS-TASK] Created Admin Panel Page

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/admin', 'AdminController@index');
+
 Route::resource('categories', 'CategoriesController');
