7d08ea758a9bec7dfa06535ccc4b6ac705253dc4
christanplaza <cosmotechnologist@gmail.com>
2019-06-21 10:55:09 +0800

    Added Button for Starting Lesson and Category Number of Words

--- resources/views/category/show.blade.php
+++ resources/views/category/show.blade.php
@@ -6,10 +6,11 @@
             <div class="card">
                 <div class="card-body">
                     <h3> {{ $category->title }}</h3>
-
-                    <p>
+                    <small class="muted">Number of words: 10</small>
+                    <p class="mt-3">
                         {{ $category->description }}
                     </p>
+                    <a href="#" class="btn btn-outline-primary float-right">Start Lesson</a>
                 </div>
             </div>
         </div>
