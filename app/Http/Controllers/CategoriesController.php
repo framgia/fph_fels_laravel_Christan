fbcf5f6b084d6c56a53869165915418cc5dc4d18
christanplaza <cosmotechnologist@gmail.com>
2019-06-21 13:43:17 +0800

    [SELS-TASK] Created Category Controller for Admin

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -19,38 +19,6 @@ class CategoriesController extends Controller
         return view('category.show', compact('category'));
     }
 
-    public function create()
-    {
-        return view('category.create');
-    }
-
-    public function store()
-    {
-        $attributes = $this->validateCategory();
-        Category::create($attributes);
-
-        return redirect('/categories');
-    }
-
-    public function edit(Category $category)
-    {
-        return view('category.edit', compact('category'));
-    }
-
-    public function update(Category $category)
-    {
-        $category->update($this->validateCategory());
-
-        return redirect('/categories');
-    }
-
-    public function destroy(Category $category)
-    {
-        $category->delete();
-
-        return redirect('/categories');
-    }
-
     protected function validateCategory(){
         return request()->validate([
             'title' => ['required', 'min:3'],
