9ceb174f72dcb0493eb6d4c5bf2bb0379f30b167
christanplaza <cosmotechnologist@gmail.com>
2019-06-24 18:46:00 +0800

    [SELS-TASK] Implemented Edit Word and Choices Function

--- app/Http/Controllers/AdminWordsController.php
+++ app/Http/Controllers/AdminWordsController.php
@@ -8,6 +8,12 @@ use App\Http\Requests\StoreWord;
 
 class AdminWordsController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('admin');
+    }
+
     public function show(Word $word)
     {
         return view ('admin.word.show', compact('word'));
@@ -26,5 +32,31 @@ class AdminWordsController extends Controller
         session()->flash('message', $attributes['text'].' has been added to ' . $category->title);
 
         return redirect('/admin/categories');
+    }
+
+    public function edit(Word $word)
+    {
+        $choices = $word->choices;
+        return view('admin.word.edit', compact('word', 'choices'));
+    }
+
+    public function update(Word $word)
+    {
+        $attributes = $this->validateEntry();
+        $attributes['word_id'] = $word->id;
+        $word->updateRecord($attributes);
+
+        return redirect('/admin/categories');
+    }
+
+    protected function validateEntry(){
+        return request()->validate([
+            'text' => ['required'],
+            'choice0' => ['required', 'min:3'],
+            'choice1' => ['required', 'min:3'],
+            'choice2' => ['required', 'min:3'],
+            'choice3' => ['required', 'min:3'],
+            'answer' => ['required'],
+        ]);
     }
 }
