aebc7d5d13351d244f5225e96a77ce84437b8a9c
christanplaza <cosmotechnologist@gmail.com>
2019-06-27 16:43:44 +0800

    [SELS-TASK] Implemented Display Results Function

--- resources/views/lessons/show.blade.php
+++ resources/views/lessons/show.blade.php
@@ -1,17 +1,52 @@
 @extends('layout')
 
 @section('content')
-<div class="row justify-content-center">
-    <div class="col-lg-4">
-        <div class="card">
+<div class="row justify-content-center mt-4">
+    <div class="col-lg-6 text-center">
+        <h1>You have finished {{ $lesson->category->title }}!</h1>
+    </div>
+</div>
+<div class="row justify-content-center mt-4">
+    <div class="col-lg-6">
+        <div class="card shadow">
             <div class="card-header">
-                Quiz
+                {{$lesson->category->title}}
+
+                <span class="float-right">Score: {{ $quiz->result }} of {{ $lesson->category->words->count() }}</span>
             </div>
             <div class="card-body">
-                    {{ $lesson->category->title }}
-                <small class="muted">This category has {{$lesson->category->words->count()}} words</small>
+                <table class="table">
+                    <thead class="thead-dark">
+                    <tr>
+                        <th>Result</th>
+                        <th>Word</th>
+                        <th>Answer</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($answers as $answer)
+                        <tr>
+                            <td>
+
+                                <i class="{{$answer->choice->is_correct == 1 ? 'far fa-circle' : 'fas fa-times'}}"></i>
+                            </td>
+                            <td>
+                                {{ $answer->choice->word->text }}
+                            </td>
+                            <td>
+                                {{ $answer->choice->text }}
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
+</div>
+<div class="row justify-content-center mt-4">
+    <a href="/categories">
+        <button class="btn btn-lg btn-primary shadow">You think you can do more? Try other Categories!</button>
+    </a>
 </div>
 @endsection
