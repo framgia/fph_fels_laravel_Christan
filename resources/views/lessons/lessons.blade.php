922364058da4711ba60da734d66a6c4dc6bb10c2
christanplaza <cosmotechnologist@gmail.com>
2019-07-03 17:00:35 +0800

    [SELS-TASK] Implemented Results List Function

--- resources/views/lessons/lessons.blade.php
+++ resources/views/lessons/lessons.blade.php
@@ -1,11 +1,26 @@
 @extends('layout')
 
 @section('content')
-<ul>
+@if($lessons->count())
+<h1 class="title">Lessons Taken</h1>
+<div class="card-deck">
     @foreach ($lessons as $lesson)
-    <li>
-        {{ $lesson->category->title }}
-    </li>
+    <div class="card shadow">
+        <div class="card-body">
+            <a href="/lessons/{{ $lesson->id }}">
+                <h5>
+                    {{ $lesson->category->title }}
+                </h5>
+            </a>
+            <p class="card-text">Score: {{ $lesson->quiz->result }} out of {{ $lesson->category->words->count() }}</p>
+            <small>Taken: {{ Carbon\Carbon::parse($lesson->created_at)->diffForHumans() }}</small>
+        </div>
+    </div>
     @endforeach
-</ul>
+</div>
+@else
+    <h1 class="title"><b>Oops!</b></h1>
+    <h3>You haven't taken any lessons yet!</h3>
+    <small>Come back when you've finished at least 1 lesson. :)</small>
+@endif
 @endsection
