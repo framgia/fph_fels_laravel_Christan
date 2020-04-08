6a3960c275a50ef430a8c4109e6b20f5db43fb88
christanplaza <christan.shane.plaza@sun-asterisk.com>
2020-04-08 18:00:51 +0800

    Heroku DB Setup

--- config/database.php
+++ config/database.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Str;
 
+$url = parse_url(getenv('DATABASE_URL'));
+$host = $url['host'] ?? null;
+$username = $url['user'];
+$password = $url['pass'];
+$database = substr($url['path'], 1);
+
 return [
 
     /*
@@ -15,7 +21,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql_production'),
 
     /*
     |--------------------------------------------------------------------------
@@ -35,6 +41,19 @@ return [
 
     'connections' => [
 
+        'pgsql_production' => [
+            'driver' => 'pgsql',
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ], 
+        
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
